cd62b0ca4772eed3f5db0ebed4dab341e403c28d
igor <vavilonskiy99@mail.ru>
2023-12-12 14:29:41 +0500

    Добавил метод наполнения массива

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
-﻿System.Console.Write("Введите размер массива");
+﻿Console.Write("Введите размер массива");
 int n = Convert.ToInt32(Console.ReadLine());
-
 string[] strings = new string[n];
+
+void FillArray(string[] str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (i == 0)
+        {
+        Console.Write("Введите что-нибудь: ");
+        str[i] = Console.ReadLine();
+        }
+        else
+        {
+        Console.Write("Введите что-нибудь ещё: ");
+        str[i] = Console.ReadLine();
+        }
+    }
+}
